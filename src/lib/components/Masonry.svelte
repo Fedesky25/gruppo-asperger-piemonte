ba2b032ffe4256a6a596a286843bb6797e74f45b
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-10-05 12:29:52 +0200

    feat: masonry component

---
+++ src/lib/components/Masonry.svelte
@@ -0,0 +1,80 @@
+<script lang="ts" generics="T extends {}, K extends keyof T">
+    import { onDestroy, type Snippet } from "svelte";
+
+    interface Props {
+        key?: K | null;
+        items: T[];
+        children: Snippet<[T, number]>;
+    }
+    let { key = null, items, children }: Props = $props();
+
+    let wrapper: HTMLDivElement;
+    let refresh_request: number | null = null;
+
+    function refresh_layout() {
+        console.log("Masonry layout refreshed");
+
+        const ncols =
+            getComputedStyle(wrapper).gridTemplateColumns.split(" ").length;
+
+        if (ncols == 1) {
+            wrapper.style.gridAutoRows = "auto";
+            wrapper.style.paddingBottom = "0";
+            for (const child of wrapper.children) {
+                (child as HTMLDivElement).style.marginTop = "0px";
+            }
+        } else {
+            const offsets = new Array<number>(ncols).fill(0);
+            let idx = 0;
+            for (const _child of wrapper.children) {
+                const child = _child as HTMLDivElement;
+                const height = child.getBoundingClientRect().height;
+                const col = idx % ncols;
+                // child.style.gridColumn = (1 + col).toString();
+                child.style.marginTop = `${offsets[col]}px`;
+                offsets[col] += height;
+                idx++;
+            }
+            wrapper.style.gridAutoRows = "0px";
+            wrapper.style.paddingBottom = `${Math.max(...offsets)}px`;
+        }
+    }
+
+    $effect(() => {
+        wrapper && items.length && refresh_layout();
+    });
+
+    onDestroy(() => {
+        if (refresh_request) clearTimeout(refresh_request);
+    });
+</script>
+
+<svelte:window
+    onresize={() => {
+        if (!refresh_request) refresh_request = setTimeout(refresh_layout, 10);
+    }}
+/>
+
+<div class="wrapper" bind:this={wrapper}>
+    {#each items as it, idx (key === null ? idx : it[key])}
+        <div class="item" data-index={idx}>
+            {@render children(it, idx)}
+        </div>
+    {/each}
+</div>
+
+<style>
+    .wrapper {
+        display: grid;
+        grid-auto-rows: 0px;
+        grid-template-columns: repeat(
+            auto-fill,
+            minmax(var(--col-width, 40ch), 1fr)
+        );
+        column-gap: var(--gap, 1em);
+    }
+    .item {
+        align-self: start;
+        padding-bottom: var(--gap, 1em);
+    }
+</style>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import Masonry from "./components/Masonry.svelte";
+import Copyable from "./components/Copyable.svelte";
+import BackLogo from "./components/BackLogo.svelte";
+
+export { Masonry, Copyable, BackLogo };
